47a75c6e99ea20f89ba40b22cf17da29929d54b6
github username <github email address>
2020-02-18 09:57:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue){
+    return drivers.filter(function(driver){
+        return driver.revenue > revenue;
+    });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){ // function that takes in drivers/revenue
+    return driversWithRevenueOver(drivers, revenue).map(function(driver){ // take whats already been done above, and map out each name that has bigger revenue than the input revenue.
+        return driver.name;
+    });
+}
+
+function exactMatch(drivers, driverObject){
+    return drivers.filter(function(driver){
+        let match = 0;
+
+        for (const key in driverObject){
+            match = driver[key] === driverObject[key];
+        }
+
+        return match;
+    });
+}
+
+function exactMatchToList(drivers, driverObject){
+    return exactMatch(drivers, driverObject).map(function(driver){ // pulls from the already "matched by key" function and pulls out their NAME.
+        return driver.name;
+    });
+}
